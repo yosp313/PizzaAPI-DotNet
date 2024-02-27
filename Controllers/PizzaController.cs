69bae4e459502fe854c1c5e711fda16eb69d3134
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-02-27 10:47:16 +0200

    Refactor PizzaController and remove WeatherForecastController and WeatherForecast

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -2,65 +2,50 @@ using WebApp_test.Services;
 using WebApp_test.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApp_test.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class PizzaController : ControllerBase
-    {
-        public PizzaController()
-        {
-        }
+namespace WebApp_test.Controllers {
+  [ApiController]
+  [Route("api/[controller]")]
+  public class PizzaController: ControllerBase {
+    public PizzaController() {}[HttpGet]
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
-        [HttpGet]
-        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id) {
+      var pizza = PizzaService.Get(id);
 
-        [HttpGet("{id}")]
-        public ActionResult<Pizza> Get(int id)
-        {
-            var pizza = PizzaService.Get(id);
+      if (pizza == null) {
+        return NotFound();
+      }
 
-            if(pizza == null)
-            {
-                return NotFound();
-            }
+      return pizza;
+    }[HttpPost]
+    public ActionResult<Pizza> Create(Pizza pizza) {
+      PizzaService.Add(pizza);
+      return CreatedAtAction(nameof(Get), new {
+        id = pizza.Id
+      }, pizza);
+    }[HttpDelete("{id}")]
+    public ActionResult<Pizza> Delete(int id) {
+      var pizza = PizzaService.Get(id);
+      if (pizza == null) {
+        return NotFound();
+      }
 
-            return pizza;
-        }
+      PizzaService.Delete(id);
+      return NoContent();
+    }[HttpPut("{id}")]
+    public ActionResult<Pizza> Update(int id, Pizza pizza) {
+      if (id != pizza.Id) {
+        return BadRequest();
+      }
 
-        [HttpPost]
-        public ActionResult<Pizza> Create(Pizza pizza){
-            PizzaService.Add(pizza);
-            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
-        }
+      var existingPizza = PizzaService.Get(id);
+      if (existingPizza is null) {
+        return NotFound();
+      }
 
-        [HttpDelete("{id}")]
-        public ActionResult<Pizza> Delete(int id){
-            var pizza = PizzaService.Get(id);
-            if(pizza == null)
-            {
-                return NotFound();
-            }
-
-            PizzaService.Delete(id);
-            return NoContent();
-        }
-
-        [HttpPut("{id}")]
-        public ActionResult<Pizza> Update(int id, Pizza pizza){
-            if(id != pizza.Id)
-            {
-                return BadRequest();
-            }
-
-            var existingPizza = PizzaService.Get(id);
-            if(existingPizza is null)
-            {
-                return NotFound();
-            }
-
-            PizzaService.Update(pizza);
-            return NoContent();
-        }
+      PizzaService.Update(pizza);
+      return NoContent();
     }
+  }
 }
